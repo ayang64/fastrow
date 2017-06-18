1fa58b3d6ebafaf57de6da405e5b2554e1e4fd59
Ayan George <ayan@ayan.net>
2017-06-18 09:20:09 -0400

    Major changes
    * Switch go Golang style comments (note that I didn't say C++ style comments. :P)
    * Hoist much of the logic out of the inner loop
    * Pre-allocate destarray

--- fastrow.go
+++ fastrow.go
@@ -5,51 +5,56 @@ import (
 	"reflect"
 )
 
+/* expects a slice of structs at the moment. */
 func Marshal(x interface{}, rows *sql.Rows) (interface{}, error) {
-	/* extract type information from x */
+	// extract type information stored in x interface
 	t := reflect.TypeOf(x)
 
-	/* determine exactly the kind of symbol t is.  right now we only handle
-	 * slices -- specifically slices of structs. */
+	// determine exactly the kind of symbol t is.  right now we only handle
+	// slices -- specifically slices of structs. */
 	switch t.Kind() {
 	case reflect.Slice:
-		colmap := make(map[string]int)
 		cols, err := rows.Columns()
 
 		if err != nil {
 			return x, err
 		}
 
-		/* store column indices for easy retrieval. */
+		// retrieve the type that TypeOf(x) points to.  So if reflect.TypeOf(x) is
+		// s slice, then reflect.TypeOf(x).Elem() will give us what x is a slice
+		// of.
+		te := t.Elem()
+
+		// get the value of x.  we'll be appending to this
+		v := reflect.ValueOf(x)
+
+		// store column indices for easy retrieval.
+		colmap := make(map[string]int)
 		for idx, col := range cols {
 			colmap[col] = idx
 		}
 
-		/* retrieve the type that TypeOf(x) points to.  So if reflect.TypeOf(x) is
-		 * s slice, then reflect.TypeOf(x).Elem() will give us what x is a slice
-		 * of. */
-		te := t.Elem()
-
-		/* get the value of x.  we'll be appendingn to this */
-		v := reflect.ValueOf(x)
-
-		/* walk our list of sql results. */
-		for rows.Next() {
-			nv := reflect.New(te)
-			destslice := []interface{}{}
-
-			/* rows.Scan() takes a variable number of interface{} values that contain
-			 * pointers to elements to pack.  we can supply that by giving
-			 * rows.Scan() a slice of interfaces{} and using the elipsis operator. */
-			for _, col := range cols {
-				for i := 0; i < te.NumField(); i++ {
-					if f := te.Field(i); col == f.Tag.Get("col") {
-						destslice = append(destslice, nv.Elem().Field(i).Addr().Interface())
-						break
-					}
-				}
+		// store field indices in the order that they're needed for retrieval.
+		var fields []int
+		for i := 0; i < te.NumField(); i++ {
+			if col, ok := te.Field(i).Tag.Lookup("col"); ok {
+				fields = append(fields, colmap[col])
 			}
+		}
 
+		destslice := make([]interface{}, len(fields), len(fields))
+
+		// walk our list of sql results.
+		nv := reflect.New(te)
+
+		for idx, field := range fields {
+			destslice[idx] = nv.Elem().Field(field).Addr().Interface()
+		}
+
+		for rows.Next() {
+			// rows.Scan() takes a variable number of interface{} values that contain
+			// pointers to elements to pack.  we can supply that by giving
+			// rows.Scan() a slice of interfaces{} and using the elipsis operator.
 			if err := rows.Scan(destslice...); err != nil {
 				return x, err
 			}
